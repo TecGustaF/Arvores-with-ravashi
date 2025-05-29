b605a0da57b9818d3e246bbb70cf45a9a9635da7
TecGustaF <168040734+TecGustaF@users.noreply.github.com>
2025-05-29 20:18:58 -0300

    Update No.java

--- No.java
+++ No.java
@@ -3,9 +3,19 @@ package no;
 public class No {
     public String valor;
     public No esquerda, direita;
+    private int fatorBalanceamento; // agora é privado
 
     public No(String valor) {
         this.valor = valor;
         esquerda = direita = null;
+        fatorBalanceamento = 0;
+    }
+
+    public int getFatorBalanceamento() {
+        return fatorBalanceamento;
+    }
+
+    public void setFatorBalanceamento(int fatorBalanceamento) {
+        this.fatorBalanceamento = fatorBalanceamento;
     }
 }
